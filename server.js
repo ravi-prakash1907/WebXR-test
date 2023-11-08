96b9e9ce11f75bd6827041ee7f5095aab5c11891
ravi-prakash1907 <1907prakash.ravi@gmail.com>
2023-11-08 11:41:34 +0530

    updates for api

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+// const fetch = require('node-fetch');
 
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, "/")));
 app.use(express.json());
+// app.use(express.import());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/data', async (req, res) => {
+    try {
+        const response = await fetch('http://0.0.0.0:8080/data');
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching data');
+    }
+});
+
 app.listen(8080, () => {
+    // console.log("Listening on http://localhost:8080");
     console.log("Listening on http://0.0.0.0:8080");
 });
+
+
+
